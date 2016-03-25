929dda2b38038378ed0e5a175d570400e48ffd27
Shrimadhav U K <spechide@shrimadhavuk.me>
2016-03-25 11:51:14 +0530

    remove hard coded values

--- api/index.php
+++ api/index.php
@@ -17,12 +17,32 @@
   <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
   <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
   <![endif]-->
+
+  <script src="../js/jquery-2.2.2.min.js" integrity="sha256-36cp2Co+/62rEAAYHLmRCPIych47CvdM+uTBJwSzWjI="></script>
+
+  <script type="text/javascript">
+
+    function getNoOfTF(){
+      var inputs = document.getElementsByTagName('input');
+      var count = 0;
+      for(var cpt = 0; cpt < inputs.length; cpt++)
+        if (inputs[cpt].type == 'text') count++;
+      return count;
+    }
+
+    function addOneMore(){
+      var requiredVal = getNoOfTF() + 1;
+      var requiredHTML = "<div class='form-group'><input tabindex='1' class='form-control' placeholder='enter feed' name='feed" + requiredVal + "' type='text' value='' autofocus></div>";
+      $(requiredHTML).insertAfter($('.form-group').last());
+    }
+  </script>
+
 </head>
 <body>
 
   <div class="container">
     <div style="height:50px;"></div>
-    <div class="row">
+
 
         <div class="login-panel panel panel-default">
           <div class="panel-heading">
@@ -31,36 +51,26 @@
           <div class="panel-body">
             <form role="form" method="POST" action="" accept-charset="UTF-8">
               <fieldset>
-                <div class="form-group">
-                  <input tabindex="1" class="form-control" placeholder="feed one" name="feed1" type="text" value="" autofocus>
-                </div>
-                <div class="form-group">
-                  <input tabindex="2" class="form-control" placeholder="feed one" name="feed2" type="text" value="">
-                </div>
-                <div class="form-group">
-                  <input tabindex="3" class="form-control" placeholder="feed one" name="feed3" type="text" value="">
-                </div>
-                <div class="form-group">
-                  <input tabindex="4" class="form-control" placeholder="feed one" name="feed4" type="text" value="">
-                </div>
-                <div class="form-group">
-                  <input tabindex="5" class="form-control" placeholder="feed one" name="feed5" type="text" value="">
-                </div>
-                <button tabindex="6" type="submit" name="sbmtbtn" class="btn btn-lg btn-success btn-block">Submit</button>
+
+                <div class='form-group'><input tabindex='1' class='form-control' placeholder='enter feed' name='feed1' type='text' value='' autofocus></div>
+
+                <button tabindex='2' type="button" name='addtxtbtn' class='btn btn-lg btn-success btn-block' onClick='addOneMore()'>Add more</button>
+
+                <button tabindex='3' type='submit' name='sbmtbtn' class='btn btn-lg btn-success btn-block'>Submit</button>
+
               </fieldset>
             </form>
           </div>
         </div>
 
 
-      </div>
+
       </div>
 
 <?php
 if(isset($_POST['sbmtbtn'])){
   $feeds = array_filter($_POST);
   $contents = implode("\n",$feeds);
-  echo $contents;
   $myfile = fopen("contents.txt", "w") or die("Unable to create file!");
   fwrite($myfile, $contents);
   fclose($myfile);
