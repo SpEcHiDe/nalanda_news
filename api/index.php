9c7d8d4aec76c3846dcd79d285025ad553cb3d94
Shrimadhav U K <spechide@shrimadhavuk.me>
2016-04-04 18:04:29 +0530

    add security for the api directory

--- api/index.php
+++ api/index.php
@@ -1,4 +1,14 @@
 <?php
+$valid_passwords = array ("USER_NAME" => "PASSWORD_IS_NOT_THE_PWD");
+$valid_users = array_keys($valid_passwords);
+$user = $_SERVER['PHP_AUTH_USER'];
+$pass = $_SERVER['PHP_AUTH_PW'];
+$validated = (in_array($user, $valid_users)) && ($pass == $valid_passwords[$user]);
+if (!$validated) {
+  header('WWW-Authenticate: Basic realm="Entry for Valid Users Only!"');
+  header('HTTP/1.0 401 Unauthorized');
+  die ("Not authorized");
+}
 echo "<!--";
 $myfile = fopen("contents.txt", "r") or die("Unable to open file!");
 echo "opened file for reading\n";
